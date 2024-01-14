9b86cb551939e65a01ff3bfb937c6768a7576972
swiftstand <officialswiftstand@gmail.com>
2024-01-14 11:38:38 +0100

    disabled google auth

--- user/arole.py
+++ user/arole.py
@@ -5,19 +5,19 @@ import os.path
 import os
 
 # for authentication
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
+# from google.auth.transport.requests import Request
+# from google.oauth2.credentials import Credentials
+# from google_auth_oauthlib.flow import InstalledAppFlow
 
-# for both
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+# # for both
+# from googleapiclient.discovery import build
+# from googleapiclient.errors import HttpError
 
-# for email 
-import base64
-from email.message import EmailMessage
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
+# # for email 
+# import base64
+# from email.message import EmailMessage
+# from email.mime.text import MIMEText
+# from email.mime.multipart import MIMEMultipart
 
 
 
@@ -181,123 +181,123 @@ def generate_content(customer_name, reset_link):
     return text, html
 
 
-def gmail_send_mail(sender_email, customer_name, customer_email, reset_link):
-    """Create and insert a draft email.
-       Print the returned draft's message and id.
-       Returns: Draft object, including draft id and message meta data.
+# def gmail_send_mail(sender_email, customer_name, customer_email, reset_link):
+#     """Create and insert a draft email.
+#        Print the returned draft's message and id.
+#        Returns: Draft object, including draft id and message meta data.
 
-      Load pre-authorized user credentials from the environment.
-      TODO(developer) - See https://developers.google.com/identity
-      for guides on implementing OAuth2 for the application.
-    """
-    print("--------------  AUTHENTICATING ----------------\n\n")
-    creds = authenticate()
-    if not creds:
-        creds = authenticate()
-    print("\n\n--------------  DONE ----------------\n\n")
+#       Load pre-authorized user credentials from the environment.
+#       TODO(developer) - See https://developers.google.com/identity
+#       for guides on implementing OAuth2 for the application.
+#     """
+#     print("--------------  AUTHENTICATING ----------------\n\n")
+#     creds = authenticate()
+#     if not creds:
+#         creds = authenticate()
+#     print("\n\n--------------  DONE ----------------\n\n")
 
-    try:
-        # create gmail api client
-        service = build('gmail', 'v1', credentials=creds)
+#     try:
+#         # create gmail api client
+#         service = build('gmail', 'v1', credentials=creds)
 
-        # message = EmailMessage()
+#         # message = EmailMessage()
 
-        message = MIMEMultipart("alternative")
-        message["Subject"] = "Arole Gamer App | Reset your password"
-        message["From"] = sender_email
-        message["To"] = customer_email
-
-
-        # generate content for the mail both html and plain format
-        message_text, message_html = generate_content(customer_name, reset_link)
-
-        # Turn these into plain/html MIMEText objects
-        part1 = MIMEText(message_text, "plain")
-        part2 = MIMEText(message_html, "html")
-
-        # Add HTML/plain-text parts to MIMEMultipart message
-        # The email client will try to render the last part first
-        message.attach(part1)
-        message.attach(part2)
+#         message = MIMEMultipart("alternative")
+#         message["Subject"] = "Arole Gamer App | Reset your password"
+#         message["From"] = sender_email
+#         message["To"] = customer_email
 
 
-        # encoded message
-        encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
+#         # generate content for the mail both html and plain format
+#         message_text, message_html = generate_content(customer_name, reset_link)
 
-        create_message = {'raw': encoded_message}
+#         # Turn these into plain/html MIMEText objects
+#         part1 = MIMEText(message_text, "plain")
+#         part2 = MIMEText(message_html, "html")
 
-        # pylint: disable=E1101
-        send_message = (service.users().messages().send
-                        (userId="me", body=create_message).execute())
-
-        print(F'Message Id: {send_message["id"]}')
-
-        success = True
-
-
-    except HttpError as error:
-        print(F'An error occurred: {error}')
-        success = False
-
-    return success
+#         # Add HTML/plain-text parts to MIMEMultipart message
+#         # The email client will try to render the last part first
+#         message.attach(part1)
+#         message.attach(part2)
 
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = ['https://mail.google.com/']
+#         # encoded message
+#         encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
 
-print("CWD : ", os.path.abspath(os.getcwd()))
+#         create_message = {'raw': encoded_message}
 
-def authenticate():
-    """Shows basic usage of the Gmail API.
-    Lists the user's Gmail labels.
-    """
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists(os.path.dirname(os.path.abspath(__file__))+'/token.json'):
-        creds = Credentials.from_authorized_user_file(os.path.dirname(os.path.abspath(__file__))+'/token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid or creds.expired:
-        # if creds and creds.expired and creds.refresh_token:
-            # try:
-                # creds.refresh(Request())
-            # with open(os.path.dirname(os.path.abspath(__file__))+'/token.json', 'w') as token:
-            #     token.write(creds.to_json())
-            # return None
-            # except:
-            #     print("REGENERATING")
-            #     os.remove(os.path.dirname(os.path.abspath(__file__))+"/token.json")
-            #     authenticate()
-        # else:
-        flow = InstalledAppFlow.from_client_secrets_file(
-            os.path.dirname(os.path.abspath(__file__))+'/credentials.json', SCOPES)
-        creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open(os.path.dirname(os.path.abspath(__file__))+'/token.json', 'w') as token:
-            token.write(creds.to_json())
+#         # pylint: disable=E1101
+#         send_message = (service.users().messages().send
+#                         (userId="me", body=create_message).execute())
 
-    try:
-        # Call the Gmail API
-        service = build('gmail', 'v1', credentials=creds)
-        results = service.users().labels().list(userId='me').execute()
-        labels = results.get('labels', [])
+#         print(F'Message Id: {send_message["id"]}')
 
-        if not labels:
-            print('No labels found.')
-            return
-
-    except HttpError as error:
-        # TODO(developer) - Handle errors from gmail API.
-        print(f'An error occurred: {error}')
-
-    return creds
+#         success = True
 
 
-def send_reset_mail(receiver, code, name):
-    sender = "aroleplaystation@gmail.com"
-    receiver_email = receiver
-    gmail_send_mail(sender_email=sender, customer_name=name, customer_email=receiver_email, reset_link=code)
+#     except HttpError as error:
+#         print(F'An error occurred: {error}')
+#         success = False
+
+#     return success
+
+
+# # If modifying these scopes, delete the file token.json.
+# SCOPES = ['https://mail.google.com/']
+
+# print("CWD : ", os.path.abspath(os.getcwd()))
+
+# def authenticate():
+#     """Shows basic usage of the Gmail API.
+#     Lists the user's Gmail labels.
+#     """
+#     creds = None
+#     # The file token.json stores the user's access and refresh tokens, and is
+#     # created automatically when the authorization flow completes for the first
+#     # time.
+#     if os.path.exists(os.path.dirname(os.path.abspath(__file__))+'/token.json'):
+#         creds = Credentials.from_authorized_user_file(os.path.dirname(os.path.abspath(__file__))+'/token.json', SCOPES)
+#     # If there are no (valid) credentials available, let the user log in.
+#     if not creds or not creds.valid or creds.expired:
+#         # if creds and creds.expired and creds.refresh_token:
+#             # try:
+#                 # creds.refresh(Request())
+#             # with open(os.path.dirname(os.path.abspath(__file__))+'/token.json', 'w') as token:
+#             #     token.write(creds.to_json())
+#             # return None
+#             # except:
+#             #     print("REGENERATING")
+#             #     os.remove(os.path.dirname(os.path.abspath(__file__))+"/token.json")
+#             #     authenticate()
+#         # else:
+#         flow = InstalledAppFlow.from_client_secrets_file(
+#             os.path.dirname(os.path.abspath(__file__))+'/credentials.json', SCOPES)
+#         creds = flow.run_local_server(port=0)
+#         # Save the credentials for the next run
+#         with open(os.path.dirname(os.path.abspath(__file__))+'/token.json', 'w') as token:
+#             token.write(creds.to_json())
+
+#     try:
+#         # Call the Gmail API
+#         service = build('gmail', 'v1', credentials=creds)
+#         results = service.users().labels().list(userId='me').execute()
+#         labels = results.get('labels', [])
+
+#         if not labels:
+#             print('No labels found.')
+#             return
+
+#     except HttpError as error:
+#         # TODO(developer) - Handle errors from gmail API.
+#         print(f'An error occurred: {error}')
+
+#     return creds
+
+
+# def send_reset_mail(receiver, code, name):
+#     sender = "aroleplaystation@gmail.com"
+#     receiver_email = receiver
+#     gmail_send_mail(sender_email=sender, customer_name=name, customer_email=receiver_email, reset_link=code)
 
 
 
